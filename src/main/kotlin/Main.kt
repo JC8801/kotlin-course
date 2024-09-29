1cfdfa78983c2bdcf250e476d3db5423762dfa84
JC_prog <aetherlyssa@gmail.com>
2024-09-29 09:26:37 +0600

    переменные

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
-val name: String = "Yakov"
+val name: String = "Yakov"       // постоянное значение
 
-var age: Int = 36
+var age: Int = 36                // переменное значение
 
-const val PI = 3.14
+const val PI = 3.14              // постоянное значение для всего проекта, глобальная постоянная
+
+lateinit var userinfo: String    // способ задания, не переменная, поздняя инициализация, всегда var
