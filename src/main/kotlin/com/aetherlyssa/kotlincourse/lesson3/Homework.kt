aa72dc9c84259b45e8aa6053027cef043d087ebb
JC_prog <aetherlyssa@gmail.com>
2024-09-29 15:31:15 +0600

    переменные

--- src/main/kotlin/com/aetherlyssa/kotlincourse/lesson3/Homework.kt
+++ src/main/kotlin/com/aetherlyssa/kotlincourse/lesson3/Homework.kt
@@ -1,7 +1,11 @@
 package com.aetherlyssa.kotlincourse.lesson3
 
 class Homework {
-    // 12345
+
+    const val event: String = "Hackathon Survival"       //   Название мероприятия, постоянное строковое
+    var date: String = "22.09.2024"                      //   Дата мероприятия, изменяемое строковое (возможны изменения)
+    var location: String = "Kazakhstan, Qostanai"        //   Место проведения, изменяемое строковое (возможна смена локации)
+    lateinit var eventbudget: Int                        //   Расчет происходит в другом месте с подключением других переменных, изменяемое числовое
+
 }
 
-1412
