41c0cc5c08951e52928f24adaeaf9bacb3a29e4a
JC_prog <aetherlyssa@gmail.com>
2024-09-29 22:02:58 +0600

    переменные

--- src/main/kotlin/com/aetherlyssa/kotlincourse/Main.kt
+++ src/main/kotlin/com/aetherlyssa/kotlincourse/Main.kt
@@ -35,4 +35,32 @@ var fuel: Double = 0.00             // ничего не понятно, но о
         if (value > 0), field + value
     }
 
+Домашнее задание 4
 
+https://docs.google.com/document/d/1T4LR71BLS9FSb6-zaTaOixGmEiyneqX7g_AQA38Gx9U/edit?usp=sharing
+
+Домашнее задание 5
+
+Домашнее задание оформляем в ветке master в пакете lesson5
+
+https://docs.google.com/document/d/1ElZbppB6xMW5B3GTOkKPhRGwxIajc4uKXNrknrDMnhM/edit?usp=sharing
+
+Домашнее задание 6
+
+Домашнее задание оформляем в ветке master в пакете lesson6
+
+https://docs.google.com/document/d/1AOg6BUNSQLLFEw4FIk7J1YL13z9rDIssqtchfEO9jyU/edit?usp=sharing
+
+Домашнее задание 7
+
+Домашнее задание оформляем в ветке master в пакете lesson7
+
+https://docs.google.com/document/d/1-603DXaieclX5N48rWtIKZY7CvBA_T6OIP6YPUHGNX8/edit?usp=sharing
+
+Домашнее задание 8
+
+Домашнее задание оформляем в ветке master в пакете lesson8
+
+Тот монстр, кто сделает все дополнительные задания включая таблицу умножения в расширенном варианте получает право пройти на лайвкодинг мимо голосования (для демонстрации этой части домашки)
+
+https://docs.google.com/document/d/1KTko0aa4D3m3fqLTRDpNWcr7ph8tRm3DKlpYxgcCKfU/edit?usp=sharing
